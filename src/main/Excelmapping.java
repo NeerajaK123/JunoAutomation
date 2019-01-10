48c29a88adbf86e27d336bbb165b8a3a38757a97
Garrepalli Sandeep <gsandeep@deloitte.com>
2019-01-10 12:46:28 +0530

    committing excelmapping-datacreation-enum

--- src/main/Excelmapping.java
+++ src/main/Excelmapping.java
@@ -114,7 +114,9 @@ public class Excelmapping {
 
 	public enum DataCreation {
 
-		NonPrescriber("NonPrescriber"), Organisation("Organisation"), Site("Site");
+		NonPrescriber("NonPrescriber"), Organisation("Organisation"), Site("Site"), Address("Address"), City(
+				"City"), App_JAMS("App_JAMS"), APP_JUPTR("APP_JUPTR"), Department("Department"), APP_Product(
+						"APP_Product"), Product("Product"), APP_Protocols("APP_Protocols"), Protocol("Protocol");
 
 		/** The value. */
 		private String value;
